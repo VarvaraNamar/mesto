96b510b12759b90004c9f4d7e3a0d6dfe7776f64
Varvara Namar <varvaranamar@yandex.ru>
2023-02-13 22:14:36 +0300

    after first check

--- scripts/index.js
+++ scripts/index.js
@@ -1,52 +1,43 @@
-let editPopup = document.querySelector('.popup');
-let editProfileButton = document.querySelector('.profile__edit-button');
-let editPopupCloseButton = document.querySelector('.popup__close-button');
-let userName = document.querySelector('.profile__name');
-let userOccupation = document.querySelector('.profile__caption');
-let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-occupation');
-let popupSubmit = document.querySelector('.popup__save-button');
-
-
-editProfileButton.addEventListener('click', function() {
-  openPopup(editPopup);
-});
-
-editPopupCloseButton.addEventListener('click', function() {
-  closePopup(editPopup);
-});
-
-popupSubmit.addEventListener('click', function() {
-  submit(editPopup);
-});
+let popupLayer = document.querySelector('.popup');// слой с фоном и попапом
+let profileEditButton = document.querySelector('.profile__edit-button');// кнопка открытия формы (ручка)
+let profileCloseButton = document.querySelector('.popup__close-button');// кнопка закрытия формы (крестик)
+let userName = document.querySelector('.profile__name');// юзер в профайле
+let userJob = document.querySelector('.profile__caption');// работа в профайле
+let formElement = document.querySelector('.popup__form');// сама форма
+let nameInput = document.querySelector('.popup__input_type_name');// инпут имя
+let jobInput = document.querySelector('.popup__input_type_occupation');// инпут работа
 
 function render() {
   nameInput.value = userName.textContent;
-  jobInput.value = userOccupation.textContent;
-};
+  jobInput.value = userJob.textContent;
+};// стартовые значения из профайла
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
   render();
-};
+};// открытие формы со стартовыми значениями
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
-  render();
-};
+};// закрытие формы 
 
-function submit(popup) {
-  popup.classList.remove('popup_opened');
-}
+function formSubmit (evt) {
+    evt.preventDefault();
+    userName.textContent = nameInput.value;
+    userJob.textContent = jobInput.value;
+    closePopup(popupLayer);
+};//отправка-сохранение формы
 
-function handleFormSubmit (evt) {
-    evt.preventDefault(); 
-      userName.textContent = nameInput.value;
-      userOccupation.textContent = jobInput.value;
-};
+profileEditButton.addEventListener('click', function() {
+  openPopup(popupLayer);
+});//слушатель на кнопку редактирования формы
 
-formElement.addEventListener('submit', handleFormSubmit);
+profileCloseButton.addEventListener('click', function() {
+  closePopup(popupLayer);
+});// слушатель на кнопку закрытия формы
+
+formElement.addEventListener('submit', formSubmit);// слушатель на форму, сохранить данные
+
 
 
 
