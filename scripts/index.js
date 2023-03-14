9bb18f259c2b9ee773c285c72642779a0f913ba0
Varvara Namar <varvaranamar@yandex.ru>
2023-03-14 16:50:39 +0300

    6 спринт

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,21 @@
-const popupProfile = document.querySelector('.popup');// попап профайла
-const profileEditButton = document.querySelector('.profile__edit-button');// кнопка открытия формы (ручка)
-const profileCloseButton = document.querySelector('.popup__close-button');// кнопка закрытия формы (крестик)
+const popupProfile = document.querySelector('.popup_type_profile');// попап профайла
+const popupAddCard = document.querySelector('.popup_type_card');// попап добавления карточки
+const popupZoomImage = document.querySelector('.popup_type_zoom-image');//попап фото приближения
+const popupList = document.querySelectorAll('.popup');//коллекция попапов
+const popupCloseButtonList = document.querySelectorAll('.popup__close-button');// кнопки закрытия форм (крестики)
+const profileEditButton = document.querySelector('.profile__edit-button');// кнопка открытия формы профайла (ручка)
 const userName = document.querySelector('.profile__name');// юзер в профайле
 const userJob = document.querySelector('.profile__caption');// работа в профайле
-const formProfile = document.querySelector('.popup__form');// сама форма
-const nameInput = document.querySelector('.popup__input_type_name');// инпут имя
-const jobInput = document.querySelector('.popup__input_type_occupation');// инпут работа
-
-const popupAddCard = document.querySelector('.popup_add-card');// попап добавления карточки
 const cardAddButton = document.querySelector('.profile__add-button');// кнопка добавления карточки
-const addCardCloseButton = document.querySelector('.popup__close-button_add-card');// кнопка закрытия формы карт
-const formAddCard = document.querySelector('.popup__form_add-card');// форма добавления карточки
-const formAddCardTitleInput = formAddCard.querySelector('.popup__input_card_title')
-const formAddCardLinkInput = formAddCard.querySelector('.popup__input_card_link')
+const formProfile = document.querySelector('.popup__form_type_profile');// форма профайла
+const formAddCard = document.querySelector('.popup__form_type_card');// форма добавления карточки
+const nameInput = document.querySelector('.popup__input_type_name');// инпут имя
+const jobInput = document.querySelector('.popup__input_type_job');// инпут работа
+const formAddCardTitleInput = formAddCard.querySelector('.popup__input_type_card-title');// инпут названия карточки
+const formAddCardLinkInput = formAddCard.querySelector('.popup__input_type_card-link');// инпут ссылки на фото карточки
 const cardsList = document.querySelector('.elements');// список с карточками
-
-const popupZoomImage = document.querySelector('.popup_zoom-card');//попап фото приближения
-const popupZoomCardCloseButton = document.querySelector('.popup__close-button_zoom-card');//кнопка закрытия зум-папопа
 const imageZoom = document.querySelector('.popup__zoom-image');//картинка попапа зум
 const titleZoom = document.querySelector('.popup__zoom-title');//описание картинки зум
-
 const cardTemplate = document.querySelector('#card-template').content;// копируем темплэйт карточки
  
 // функции
@@ -94,13 +90,38 @@ function renderProfilePopup() {
   jobInput.value = userJob.textContent; 
 };// стартовые значения из профайла 
 
+function pushButtonEscape(evt){
+  if (evt.key === 'Escape') {
+    const popupClosest = document.querySelector('.popup_opened');
+    closePopup(popupClosest);
+  };
+};//функция закрытия на эскейп
+
 function openPopup(popup) { 
   popup.classList.add('popup_opened'); 
+  document.addEventListener('keydown',pushButtonEscape);//слушатель на клаву
 };// открытие формы со стартовыми значениями 
 
 function closePopup(popup) { 
   popup.classList.remove('popup_opened'); 
+  document.removeEventListener('keydown',pushButtonEscape);
 };// закрытие формы  
+
+popupList.forEach((item) => { 
+  item.addEventListener('click', (evt) => {
+    if (evt.target === item) {
+      closePopup(item);
+    };
+  });
+})//закрытие по клику на слое вне попапа
+
+popupCloseButtonList.forEach((button) => {
+  button.addEventListener('click', function() {
+    const popupClosest = button.closest('.popup_opened');
+    closePopup(popupClosest);
+    formAddCard.reset();
+  });
+})// закрытие попапов по крестику
 
 
 //слушатели
@@ -110,10 +131,6 @@ profileEditButton.addEventListener('click', function() {
   renderProfilePopup(); 
 });//слушатель на кнопку редактирования формы 
 
-profileCloseButton.addEventListener('click', function() { 
-  closePopup(popupProfile); 
-});// слушатель на кнопку закрытия формы 
-
 formProfile.addEventListener('submit', submitFormProfile);// слушатель на форму, сохранить данные 
 
 cardAddButton.addEventListener('click', function() { 
@@ -122,14 +139,7 @@ cardAddButton.addEventListener('click', function() {
 
 formAddCard.addEventListener('submit', addCard)//слушатель на форму добавления карточки 
 
-popupZoomCardCloseButton.addEventListener('click',function() { 
-  closePopup(popupZoomImage); 
-});//слушатель на закрытие картинки
 
-addCardCloseButton.addEventListener('click', function() { 
-  closePopup(popupAddCard); 
-  formAddCard.reset()
-});// слушатель на кнопку закрытия формы 
 
 
 
